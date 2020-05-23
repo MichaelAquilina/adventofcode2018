5127bb444286a49f105f5a5bd7956e24ac36faf4
Michael Aquilina <michael.aquilina@snyk.io>
2020-05-23 20:53:40 +0100

    Split into separate rect.rs file

--- day3pt1/src/main.rs
+++ day3pt1/src/main.rs
@@ -1,111 +1,18 @@
 // https://adventofcode.com/2018/day/3
+use std::error::Error;
+use std::io::{self, Read};
 
-#[derive(PartialEq, Debug)]
-struct Rect {
-    id: String,
-    x: i32,
-    y: i32,
-    width: i32,
-    height: i32,
-}
+mod rect;
 
-#[derive(PartialEq, Debug)]
-enum RectError {
-    Parse(std::num::ParseIntError),
-    Missing(String),
-}
+use rect::Rect;
 
-impl From<std::num::ParseIntError> for RectError {
-    fn from(err: std::num::ParseIntError) -> Self {
-        RectError::Parse(err)
-    }
-}
+fn main() -> Result<(), Box<dyn Error>> {
+    let mut input = String::new();
+    io::stdin().read_to_string(&mut input)?;
 
-impl RectError {
-    fn is_missing(message: &str) -> Self {
-        RectError::Missing(String::from(message))
-    }
-}
-
-impl std::str::FromStr for Rect {
-    type Err = RectError;
-
-    fn from_str(s: &str) -> Result<Self, Self::Err> {
-        let mut tokens = s.split("@");
-
-        let id = String::from(tokens.next().ok_or(RectError::is_missing("id"))?.trim());
-
-        let definition = tokens.next().ok_or(RectError::is_missing("definition"))?;
-
-        let mut tokens = definition.split(":");
-
-        let mut coordinates = tokens
-            .next()
-            .ok_or(RectError::is_missing("coordinates"))?
-            .trim()
-            .split(",");
-
-        let mut dimensions = tokens
-            .next()
-            .ok_or(RectError::is_missing("dimensions"))?
-            .trim()
-            .split("x");
-
-        let x: i32 = coordinates
-            .next()
-            .ok_or(RectError::is_missing("X position"))?
-            .parse()?;
-        let y: i32 = coordinates
-            .next()
-            .ok_or(RectError::is_missing("Y position"))?
-            .parse()?;
-
-        let width = dimensions
-            .next()
-            .ok_or(RectError::is_missing("width"))?
-            .parse()?;
-        let height = dimensions
-            .next()
-            .ok_or(RectError::is_missing("height"))?
-            .parse()?;
-
-        Ok(Rect {
-            id,
-            x,
-            y,
-            width,
-            height,
-        })
-    }
-}
-
-fn main() {
-    println!("Hello, world!");
-}
-
-#[cfg(test)]
-mod test_rect {
-    use super::*;
-
-    #[test]
-    fn test_correct_value() -> Result<(), RectError> {
-        let result: Rect = "#123 @ 3,2: 5x4".parse()?;
-
-        let expected = Rect {
-            id: String::from("#123"),
-            x: 3,
-            y: 2,
-            width: 5,
-            height: 4,
-        };
-
-        assert_eq!(result, expected);
-        Ok(())
+    for line in input.lines() {
+        let rect: Rect = line.parse()?;
     }
 
-    #[test]
-    fn test_missing_definition() {
-        let result = "".parse::<Rect>();
-        assert_eq!(result, Err(RectError::is_missing("definition")));
-    }
+    Ok(())
 }
---
+++ day3pt1/src/rect.rs
@@ -0,0 +1,134 @@
+#[derive(PartialEq, Debug)]
+pub struct Rect {
+    id: String,
+    x: i32,
+    y: i32,
+    width: i32,
+    height: i32,
+}
+
+#[derive(PartialEq, Debug)]
+pub enum RectError {
+    Parse(std::num::ParseIntError),
+    Missing(String),
+}
+
+impl std::fmt::Display for RectError {
+    fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
+        match self {
+            RectError::Parse(e) => write!(f, "Parse Error: {}", e),
+            RectError::Missing(e) => write!(f, "Missing Error: {}", e),
+        }
+    }
+}
+
+impl std::error::Error for RectError {}
+
+impl From<std::num::ParseIntError> for RectError {
+    fn from(err: std::num::ParseIntError) -> Self {
+        RectError::Parse(err)
+    }
+}
+
+impl RectError {
+    fn is_missing(message: &str) -> Self {
+        RectError::Missing(String::from(message))
+    }
+}
+
+impl std::str::FromStr for Rect {
+    type Err = RectError;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let mut tokens = s.split("@");
+
+        let id = String::from(tokens.next().ok_or(RectError::is_missing("id"))?.trim());
+
+        let definition = tokens.next().ok_or(RectError::is_missing("definition"))?;
+
+        let mut tokens = definition.split(":");
+
+        let mut coordinates = tokens
+            .next()
+            .ok_or(RectError::is_missing("coordinates"))?
+            .trim()
+            .split(",");
+
+        let x: i32 = coordinates
+            .next()
+            .ok_or(RectError::is_missing("X position"))?
+            .parse()?;
+        let y: i32 = coordinates
+            .next()
+            .ok_or(RectError::is_missing("Y position"))?
+            .parse()?;
+
+        let mut dimensions = tokens
+            .next()
+            .ok_or(RectError::is_missing("dimensions"))?
+            .trim()
+            .split("x");
+
+        let width = dimensions
+            .next()
+            .ok_or(RectError::is_missing("width"))?
+            .parse()?;
+        let height = dimensions
+            .next()
+            .ok_or(RectError::is_missing("height"))?
+            .parse()?;
+
+        Ok(Rect {
+            id,
+            x,
+            y,
+            width,
+            height,
+        })
+    }
+}
+
+#[cfg(test)]
+mod test_rect {
+    use super::*;
+    use rstest::rstest;
+
+    #[test]
+    fn test_correct_value() -> Result<(), RectError> {
+        let result: Rect = "#123 @ 3,2: 5x4".parse()?;
+
+        let expected = Rect {
+            id: String::from("#123"),
+            x: 3,
+            y: 2,
+            width: 5,
+            height: 4,
+        };
+
+        assert_eq!(result, expected);
+        Ok(())
+    }
+
+    #[test]
+    fn test_parse_error() {
+        let result = "#300 @ d,10 : 200,300".parse::<Rect>();
+        assert!(match result {
+            Err(RectError::Parse(_)) => true,
+            Err(_) => false,
+            Ok(_) => false,
+        });
+    }
+
+    #[rstest(
+        input,
+        message,
+        case("", "definition"),
+        case("#123", "definition"),
+        case("#123 @ 11", "Y position"),
+        case("#123 @ 11,12 : 100", "height")
+    )]
+    fn test_missing(input: &str, message: &str) {
+        let result = input.parse::<Rect>();
+        assert_eq!(result, Err(RectError::is_missing(message)));
+    }
+}
